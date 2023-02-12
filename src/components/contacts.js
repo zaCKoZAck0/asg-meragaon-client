21b632ae96bdef2be5503f0ac8728a2151293a6b
Ayush <ayushyadavkr0@tuta.io>
2023-02-12 22:07:44 +0530

    CHANGES

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,10 +1,10 @@
 import { Button } from "react-bootstrap"
 import { ContactBody } from "./contactBody"
 
-export const Contacts = (contacts,refresh,setRefresh) => {
+export const Contacts = (contacts) => {
     return(<div style={{maxHeight:"90vh", overflowY:"scroll"}}>
         <h2 className="text-center">Contacts</h2>
-
+        {(!contacts)?<p className="text-secondary">No Contacts</p>:''}
         {
             contacts.contacts.map(contact => <ContactBody key={contact.id} contact={contact} />)
         }
